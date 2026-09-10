64bce574268d972077d558391af826249c522e3f
taohid-hasan <taohidhasan997@outlook.com>
2026-09-10 22:47:54 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl text-blue-400">DevStack</h1>
+      <button className="btn btn-success">Success</button>
+    </>
+  );
+}
+
+export default App
