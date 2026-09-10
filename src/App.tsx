8e3f59d99a20df2e24f79144eef291e833b2b4a3
taohid-hasan <taohidhasan997@outlook.com>
2026-09-11 00:59:06 +0600

    create basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Nav from "./components/Nav";
+
 function App() {
 
   return (
     <>
-      <h1 className="text-5xl text-blue-400">DevStack</h1>
-      <button className="btn btn-success">Success</button>
+      <Nav />
     </>
   );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,28 @@
+import Logo from '../assets/logo-text.png';
+
+const Nav = () => {
+  return (
+    <nav className="">
+      <div className="container mx-auto flex justify-between px-20 py-5">
+        <img src={Logo} alt="" />
+
+        <ul className="flex gap-4 items-center">
+          <li>home</li>
+          <li>Technologies</li>
+          <li>Projects</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+
+        <div>
+          <button className="btn bg-transparent border-none shadow-none">Sign In</button>
+          <button className="btn btn-active btn-secondary rounded-[50px]">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
